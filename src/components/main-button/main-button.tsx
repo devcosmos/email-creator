e40c25a991bf4e05d52ed5407db70a4e98ccab79
cosmos <va-litv@yandex.ru>
2023-09-15 23:32:58 +0300

    create main button component

---
+++ src/components/main-button/main-button.tsx
@@ -0,0 +1,62 @@
+import { Col, FormGroup, Input, Label, Row } from 'reactstrap';
+import { ChangeEvent } from 'react';
+
+type MainButtonProps = {
+  title: string;
+  url: string;
+  addButton: boolean;
+  handleCheckboxChange: (addButton: boolean) => void;
+  handleInputChange: (evt: ChangeEvent<HTMLInputElement>) => void;
+}
+function MainButton({ title, url, addButton, handleCheckboxChange, handleInputChange }: MainButtonProps): JSX.Element {
+  return (
+    <>
+      <FormGroup switch>
+        <Input
+          type="switch"
+          checked={addButton}
+          name="addButton"
+          onChange={() => handleCheckboxChange(!addButton)}
+        />
+        <Label check>Добавить кнопку?</Label>
+      </FormGroup>
+
+      {addButton && (
+        <Row className="mt-4">
+          <Col md={6}>
+            <FormGroup>
+              <Label for="mainButtonTitle">
+                Текст кнопки
+              </Label>
+              <Input
+                id="mainButtonTitle"
+                name="mainButtonTitle"
+                type="text"
+                onChange={handleInputChange}
+                value={title}
+                required
+              />
+            </FormGroup>
+          </Col>
+          <Col md={6}>
+            <FormGroup>
+              <Label for="mainButtonUrl">
+                Ссылка
+              </Label>
+              <Input
+                id="mainButtonUrl"
+                name="mainButtonUrl"
+                type="url"
+                onChange={handleInputChange}
+                value={url}
+                required
+              />
+            </FormGroup>
+          </Col>
+        </Row>
+      )}
+    </>
+  );
+}
+
+export default MainButton;

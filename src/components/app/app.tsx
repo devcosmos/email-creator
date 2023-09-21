df1e9f47973ef0f18547b55edef54904a4afe480
manux21 <manux21@yandex.ru>
2023-09-22 00:27:00 +0300

    feat: template configuration features

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,12 +1,28 @@
-import { ChangeEvent, useRef, useState } from 'react';
-import { Card, CardBody, CardHeader, Col, Container, Row } from 'reactstrap';
+import { ChangeEvent, useRef, useState} from 'react';
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Col,
+  Container,
+  Row,
+  Button,
+  Modal,
+  ModalBody,
+  ModalHeader,
+  Form,
+  FormGroup,
+  Label,
+} from 'reactstrap';
 import UploadImageInput from '../upload-image-input/upload-image-input';
+import UploadJsonInput from '../upload-json-input/upload-json-input';
 import EditorInput from '../editor-input/editor-input';
 import BaseDod from '../email-template/base-dod';
 import TitleInput from '../title-input/title-input';
 import SaveButtons from '../save-buttons/save-buttons';
 import { LetterData } from '../../types/data';
 import MainButton from '../main-button/main-button';
+import {letterValues} from '../../helpers/objects';
 
 /*
 Идеи/Баги:
@@ -18,21 +34,49 @@ import MainButton from '../main-button/main-button';
 */
 
 function App(): JSX.Element {
+
   const letter = useRef<null | HTMLDivElement>(null);
 
-  const [letterData, setLetterData] = useState<LetterData>({
-    title: 'Привет!',
-    image: null,
-    body: 'Здесь могла бы быть ваша реклама...',
-    addButton: false,
-    mainButtonTitle: 'Подробнее',
-    mainButtonUrl: 'https://mai.ru/',
-  });
+  const [letterData, setLetterData] = useState<LetterData>(letterValues);
 
-  const handleInputChange = (evt: ChangeEvent<HTMLInputElement>) => {
+  const [modal, setModal] = useState(false);
+
+  const handleInputChange = (evt: ChangeEvent<null | HTMLInputElement>) => {
     const {name, value} = evt.target;
 
     setLetterData({...letterData, [name]: value});
+  };
+
+  const modalHandler = ():void => {
+    setModal(!modal);
+  };
+
+  const downloadFile = (data: string, fileName: string, fileType: string):void => {
+    const blob = new Blob([JSON.stringify(letterData)], { type: fileType });
+    const a = document.createElement('a');
+    a.download = fileName;
+    a.href = window.URL.createObjectURL(blob);
+    const clickEvt = new MouseEvent('click', {
+      view: window,
+      bubbles: true,
+      cancelable: true,
+    });
+    a.dispatchEvent(clickEvt);
+    a.remove();
+  };
+
+  const exportJSON = ():void => {
+    const data: string = JSON.stringify(letterData);
+    downloadFile(
+      data,
+      'letter.json',
+      'text/json',
+    );
+  };
+
+  const resetData = ():void => {
+    console.log('Сбросилось');
+    setLetterData(letterValues);
   };
 
   return (
@@ -40,11 +84,30 @@ function App(): JSX.Element {
       className="bg-white border vh-100"
       fluid
     >
+      <Modal isOpen={modal}>
+        <ModalHeader toggle={modalHandler}>Изменить настройки шаблона</ModalHeader>
+        <ModalBody>
+          <Form>
+            <FormGroup className="mb-4">
+              <Label className="w-100">Сброс настроек</Label>
+              <Button onClick={() => resetData()} color="primary" outline>Сбросить</Button>
+            </FormGroup>
+            <FormGroup className="mb-4">
+              <Label className="w-100">Сохранить настройки</Label>
+              <Button color="primary" outline onClick={() => exportJSON()}>Скачать</Button>
+            </FormGroup>
+            <UploadJsonInput onFileRead={(jsonData) => setLetterData(jsonData ? jsonData : letterData)} letterData={letterData} />
+          </Form>
+        </ModalBody>
+      </Modal>
       <Row className="py-5 my-5">
         <Col xs="6">
           <Card>
             <CardHeader className="fw-bold h5 py-3">
               Настройки шаблона
+              <Button color="primary" className="ms-3" onClick={() => modalHandler()}>
+                Изменить
+              </Button>
             </CardHeader>
             <CardBody>
               <UploadImageInput setImage={(image) => setLetterData({...letterData, 'image': image})} />
@@ -76,5 +139,4 @@ function App(): JSX.Element {
     </Container>
   );
 }
-
 export default App;
---
+++ src/helpers/objects.ts
@@ -0,0 +1,10 @@
+import { LetterData } from '../types/data';
+
+export const letterValues:LetterData = {
+  title: 'Привет!',
+  image: null,
+  body: 'Здесь могла бы быть ваша реклама...',
+  addButton: false,
+  mainButtonTitle: 'Подробнее',
+  mainButtonUrl: 'https://mai.ru/',
+};

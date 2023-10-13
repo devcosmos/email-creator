85c73e63853e69d95e506c18a1404b7fe7ba5882
cosmos <va-litv@yandex.ru>
2023-10-13 16:25:23 +0300

    feat: add template hero

---
+++ src/components/template-hero/template-hero.tsx
@@ -0,0 +1,41 @@
+type TemplateHeroProps = {
+  title: string;
+  image: string;
+}
+
+function TemplateHero({ title, image }: TemplateHeroProps): JSX.Element {
+  return (
+    <tr style={{ margin: 0, padding: 0 }}>
+      <td style={{ margin: 0, padding: 0 }}>
+        <table
+          align="center"
+          border={0}
+          cellSpacing={0}
+          cellPadding={0}
+          role="presentation"
+          style={{
+            minWidth: '200px',
+            marginTop: '3%',
+            marginLeft: '3%',
+            marginRight: '3%',
+            // marginBottom: '2.5%',
+          }}
+        >
+          <tbody>
+            <tr style={{ margin: 0, padding: 0 }}>
+              <td align="center" style={{ margin: 0, padding: 0 }}>
+                <img
+                  src={image}
+                  alt={title}
+                  style={{ borderRadius: '30px', width: '100%' }}
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </td>
+    </tr>
+  );
+}
+
+export default TemplateHero;
